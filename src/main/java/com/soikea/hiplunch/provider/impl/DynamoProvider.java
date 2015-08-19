990cbaa06f060d94b3cbf47c0408e389d4c0900c
Mika Pennanen <mika.pennanen@soikea.com>
2015-08-19 15:11:47 +0300

    Add Sodexo JAMK Dynamo provider

---
+++ src/main/java/com/soikea/hiplunch/provider/impl/DynamoProvider.java
@@ -0,0 +1,24 @@
+package com.soikea.hiplunch.provider.impl;
+
+import com.soikea.hiplunch.provider.BaseSodexoProvider;
+
+/**
+ * @author Mika Pennanen, Soikea Solutions Oy, 19.8.15.
+ */
+public class DynamoProvider extends BaseSodexoProvider {
+
+    @Override
+    protected String getMessageUrl() {
+        return "http://www.sodexo.fi/jamk-dynamo/";
+    }
+
+    @Override
+    protected String getPrefix() {
+        return "Sodexo Dynamo";
+    }
+
+    @Override
+    protected String getSodexoId() {
+        return "5865";
+    }
+}
