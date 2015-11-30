8c181e42aefb11d4769956ba1c78159be771a3f1
Mika Pennanen <mika.pennanen@soikea.com>
2015-11-30 13:23:32 +0200

    Disable Mattilanniemi area restaurants from default run

--- src/main/java/com/soikea/hiplunch/Constants.java
+++ src/main/java/com/soikea/hiplunch/Constants.java
@@ -9,6 +9,6 @@ public class Constants {
 	public static final String HIP_API_KEY = "UBZKUrMSMlSv4PjCfJceixwOkR2sdByirfaRNtco"; // Soikea
 
 	public static final String[] HIGHLIGHTS = {"pekoni", "olut", "kalja", "pizza"};
-	public static final String[] ENABLED_PROVIDERS = {"mattilanniemi", "wilhelmiina", "piato", "dynamo", "fiilu"};
+	public static final String[] ENABLED_PROVIDERS = {"dynamo", "fiilu"};
 
 }
