d643c14d926a75c5106c959facd23f1b5934bd70
Mika Pennanen <mika.pennanen@soikea.com>
2016-09-21 10:28:30 +0300

    Change Nurkka feed
    Changed to support new site for Nurkka

--- src/main/java/com/soikea/hiplunch/provider/impl/NurkkaProvider.java
+++ src/main/java/com/soikea/hiplunch/provider/impl/NurkkaProvider.java
@@ -5,8 +5,6 @@ import com.soikea.hiplunch.FeedCutter;
 import com.soikea.hiplunch.StringHelper;
 import com.soikea.hiplunch.provider.Provider;
 
-import java.util.Arrays;
-
 public class NurkkaProvider extends Provider {
 
     @Override
@@ -18,13 +16,12 @@ public class NurkkaProvider extends Provider {
         String tomorrow = StringHelper.capitalize(StringHelper.getWeekdayName(1));
 
         return FeedCutter.builder(feed)
-            .withStartPoints(today, "Nurkan lounas")
-            .withEndPoints(tomorrow, "Päivän lounas ")
-            .withRemovables("\\n", "&nbsp;", "<.+?>")
+            .withStartPoints(today, "LOUNAALLA")
+            .withEndPoints(tomorrow, "Sis.")
+            .withRemovables("\\n", "&nbsp;", "<.+?>", "\\d\\d?\\.\\d\\d?\\.\\:")
             .startProcess()
-            .replace(" ", Arrays.asList("</p>", "</h5>"))
             .cleanUp()
-            .toString();
+            .toString().trim();
     }
 
     @Override
@@ -34,7 +31,7 @@ public class NurkkaProvider extends Provider {
 
     @Override
     protected String getMessageUrl() {
-        return "http://www.lutakonnurkka.fi/fi/";
+        return "http://www.lutakonnurkka.fi/lounas/";
     }
 
     @Override
