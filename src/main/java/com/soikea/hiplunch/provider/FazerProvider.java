2ba919d0f03cc3c31539cf742df8f91ee64157dd
Mika Pennanen <penni@kapsi.fi>
2018-10-11 13:25:27 +0300

    Change Fiilu feed against new fazerfoodco.fi site

--- src/main/java/com/soikea/hiplunch/provider/FazerProvider.java
+++ src/main/java/com/soikea/hiplunch/provider/FazerProvider.java
@@ -1,27 +1,54 @@
 package com.soikea.hiplunch.provider;
 
 import com.soikea.hiplunch.util.ContentUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
-import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public abstract class FazerProvider extends Provider {
 
     protected abstract String getFazerId();
 
-    private String getRssUrl() {
-        return MessageFormat
-            .format("https://www.fazer.fi/api/location/menurss/current?pageId={0}&language=fi", getFazerId());
+    private String getUrl() {
+        return String
+            .format("https://www.fazerfoodco.fi/api/restaurant/menu/day?date=%s&language=fi&restaurantPageId=%s",
+                    new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime()),
+                    getFazerId());
     }
 
     protected String readRawFeed() {
         StringBuilder stringBuilder = new StringBuilder();
 
-        String feedResults = ContentUtil.getRSSFeedResults(getRssUrl());
+        try {
+            JSONArray menus = new JSONObject(ContentUtil.getUrlContents(getUrl()))
+                    .getJSONObject("LunchMenu")
+                    .getJSONArray("SetMenus");
+            for (int i = 0; i < menus.length(); i++) {
 
-        if (feedResults.length() < 10) {
+                JSONObject menu = (JSONObject) menus.get(i);
+                String cat = menu.getString("Name");
+
+                List<String> mealStrings = new ArrayList<>();
+                JSONArray meals = menu.getJSONArray("Meals");
+                for (int j = 0; j < meals.length(); j++) {
+                    JSONObject meal = (JSONObject) meals.get(j);
+                    mealStrings.add(meal.getString("Name"));
+                }
+                stringBuilder.append(String.format("%s: %s. ", cat, StringUtils.join(mealStrings, ", ")));
+            }
+
+        } catch (JSONException e) {
+            log.error("Unable to process feed " + e.getMessage(), e);
+        }
+
+        if (stringBuilder.length() < 10) {
             stringBuilder.append(ContentUtil.ERROR_NOT_AVAILABLE);
-        } else {
-            stringBuilder.append(feedResults);
         }
         return stringBuilder.toString();
     }
--- src/main/java/com/soikea/hiplunch/provider/impl/FiiluProvider.java
+++ src/main/java/com/soikea/hiplunch/provider/impl/FiiluProvider.java
@@ -1,10 +1,6 @@
 package com.soikea.hiplunch.provider.impl;
 
-import com.soikea.hiplunch.util.FeedCutter;
-import com.soikea.hiplunch.util.StringHelper;
 import com.soikea.hiplunch.provider.FazerProvider;
-
-import java.util.Arrays;
 
 public class FiiluProvider extends FazerProvider {
 
@@ -15,7 +11,7 @@ public class FiiluProvider extends FazerProvider {
 
     @Override
     protected String getMessageUrl() {
-        return "https://www.fazer.fi/kahvilat-ja-leipomot/ravintolat/byfazer/ravintola-fiilu/";
+        return "https://www.fazerfoodco.fi/fiilu";
     }
 
     @Override
@@ -25,35 +21,11 @@ public class FiiluProvider extends FazerProvider {
 
     @Override
     protected String getFazerId() {
-        return "29801";
+        return "231260";
     }
 
     @Override
     protected String processFeed() {
-        String feed = readRawFeed();
-
-        String today =  StringHelper.getWeekdayName(0);
-        String tomorrow = StringHelper.getWeekdayName(1);
-
-        feed = FeedCutter.builder(feed)
-            .withStartPoints(today, "jälkiruoan ja kahvin.")
-            .withEndPoints(tomorrow, "end ngRepeat: lunchMenu in lunchMenus", "Lounas 7,20 opiskelijakortilla")
-            .withSpaceables("\\s\\s+?")
-            .withRemovables("\\n", "&nbsp;", "<.+?>", "\\(.+?\\)", "€")
-            .startProcess()
-            .replace(":</i> ", Arrays.asList("\\s\\d+?,\\d+"))
-            .replace(" <i>", Arrays.asList("\\n"))
-            .replace(", ", Arrays.asList("  "))
-            .replace(">", Arrays.asList(">,"))
-            .replace(" <", Arrays.asList(",\\s?<"))
-            .cleanUp()
-            .toString();
-
-        feed = feed.replaceAll("Deli salaattibaari:", "");
-        feed = feed.replaceAll("Päivän keittolounas", "Keitto");
-        feed = feed.replaceAll("Nordic Buffet", "Buffet");
-        feed = feed.replaceAll("Street gourmet grilli", "Grilli");
-
-        return feed;
+        return readRawFeed();
     }
 }
