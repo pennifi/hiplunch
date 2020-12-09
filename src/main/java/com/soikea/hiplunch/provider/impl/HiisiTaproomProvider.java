bbcf7a3ece6db231479d57bceb399e70636a159d
Mika Pennanen <penni@kapsi.fi>
2020-12-09 10:21:31 +0200

    Add Hiisi Taproom

---
+++ src/main/java/com/soikea/hiplunch/provider/impl/HiisiTaproomProvider.java
@@ -0,0 +1,49 @@
+package com.soikea.hiplunch.provider.impl;
+
+import com.soikea.hiplunch.provider.MenuProvider;
+import com.soikea.hiplunch.provider.Provider;
+import com.soikea.hiplunch.util.ContentUtil;
+import com.soikea.hiplunch.util.FeedCutter;
+import com.soikea.hiplunch.util.StringHelper;
+import org.apache.commons.lang3.StringUtils;
+
+@MenuProvider
+public class HiisiTaproomProvider extends Provider {
+
+    @Override
+    protected String processFeed() {
+
+        String feed = ContentUtil.getUrlContents(getMessageUrl());
+
+        String today = StringUtils.upperCase(StringHelper.getWeekdayName(0)).substring(0, 2);
+        String tomorrow = StringUtils.upperCase(StringHelper.getWeekdayName(1)).substring(0, 2);
+
+        log.debug(today + " " + tomorrow);
+
+        feed = FeedCutter.builder(feed)
+            .withStartPoints("Lounas Ke-Pe 10.30-14.00", today)
+            .withEndPoints(tomorrow, "Bottleshop: Aukioloajat")
+            .withSpaceables("\\s\\s+?", "\\(.+?\\)", "\\*")
+            .withRemovables("\\n", "&nbsp;", "<.+?>", "\\d+\\.\\d+")
+            .startProcess()
+            .cleanUp()
+            .toString().trim();
+
+        return feed;
+    }
+
+    @Override
+    public String getId() {
+        return "hiisi-taproom";
+    }
+
+    @Override
+    protected String getMessageUrl() {
+        return "https://hiisi.beer/taproom/";
+    }
+
+    @Override
+    public String getName() {
+        return "Hiisi Taproom";
+    }
+}
---
+++ src/test/java/com/soikea/hiplunch/provider/impl/HiisiTaproomProviderTest.java
@@ -0,0 +1,5 @@
+package com.soikea.hiplunch.provider.impl;
+
+import com.soikea.hiplunch.provider.ProviderTest;
+
+public class HiisiTaproomProviderTest extends ProviderTest<HiisiTaproomProvider> {}
